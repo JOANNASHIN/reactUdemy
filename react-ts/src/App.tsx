4ec2f6c1e21057ec9481033ba666a242215ee176
JOANNASHIN <joannashin3@gmail.com>
2023-05-12 17:49:43 +0900

    todo 입력 컴포넌트

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,15 +1,25 @@
+import { useState } from 'react';
 import './App.css';
+import NewTodo from './components/NewTodo';
 import Todos from './components/Todos';
 import Todo from './models/todo';
 
 function App() {
-  const todos = [
-    new Todo('Learn React'),
-    new Todo('Learn Typescript'),
-  ]
+  const [ todos, setTodos ] = useState<Todo[]>([]);
+
+  /**
+   * todo 추가
+   * 
+   * @param todoText 입력한 text
+   */
+  const addTodoHandler = (todoText: string) => {
+    const newTodo = new Todo(todoText);
+    setTodos([...todos, newTodo]);
+  }
 
   return (
     <div>
+      <NewTodo onAddTodo={addTodoHandler}/>
       <Todos items={todos} />
     </div>
   );
---
+++ react-ts/src/components/NewTodo.tsx
@@ -0,0 +1,47 @@
+import { ChangeEvent, useRef, useState } from 'react';
+
+const NewTodo:React.FC<{ onAddTodo: (text: string) => void }> = (props) => {
+    const todoTextInputRef = useRef<HTMLInputElement>(null);
+    const [ inputValue, setInputValue ] = useState<string>('');
+
+    /**
+     * update input value
+     */
+    const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setInputValue((event.target as HTMLInputElement)?.value ?? '');
+    }
+
+    /**
+     * submit form
+     */
+    const submitHandler = (event: React.FormEvent) => {
+        event.preventDefault();
+
+        const enteredText = todoTextInputRef.current!.value;
+        if (enteredText.trim().length === 0) {
+            return;
+        }
+
+        // event 전달
+        props.onAddTodo(enteredText);
+
+        // input 값 reset
+        setInputValue('');
+    }
+
+    return (
+        <form onSubmit={submitHandler}>
+            <label htmlFor="text">Todo text</label>
+            <input 
+                type="text"
+                id='text'
+                ref={todoTextInputRef}
+                value={inputValue} 
+                onChange={handleInputChange}
+            />
+            <button type="submit">Add Todo</button>
+        </form>
+    )
+}
+
+export default NewTodo;
