a45713117851aebc7897ee66c69c16b125e42743
JOANNASHIN <joannashin3@gmail.com>
2023-05-12 17:11:54 +0900

    React.FC사용하여 props 선언

---
+++ react-ts/src/components/Todos.tsx
@@ -0,0 +1,22 @@
+import { ReactNode } from "react";
+import Todo from "../models/todo";
+import TodoItem from "./TodoItem";
+
+/**
+ * FC 는 functional component라는 뜻
+ * v18 부터는 children이 필요한지 명확하게 나타내기 위해서 children 필요하면 명시해주어야함
+ */
+interface Props {
+    items: Todo[];
+    children?: ReactNode;
+}
+
+const Todos:React.FC<Props> = (props) => {
+    return <ul>
+        {props.items?.length && props.items.map(item => (
+          <TodoItem item={item} key={item.id}/>
+        ))}
+    </ul>
+}
+
+export default Todos;
