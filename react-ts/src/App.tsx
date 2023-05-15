3debed96ab6f4bf3acd6e656de4b48bef94aa843
JOANNASHIN <joannashin3@gmail.com>
2023-05-15 17:30:18 +0900

    소스 정리

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -2,40 +2,9 @@ import './App.css';
 import NewTodo from './components/NewTodo';
 import Todos from './components/Todos';
 import TodosContextProvider from './store/todos-context';
-// import Todo from './models/todo';
-// import TodosContextProvider from './store/todos-context';
 
 function App() {
-  // const [ todos, setTodos ] = useState<Todo[]>([]);
-
-  // /**
-  //  * todo 추가 이벤트
-  //  * 
-  //  * @param todoText 입력한 text
-  //  */
-  // const addTodoHandler = (todoText: string) => {
-  //   const newTodo = new Todo(todoText);
-  //   setTodos([...todos, newTodo]);
-  // }
-
-  // /**
-  //  * todo 삭제 이벤트
-  //  */
-  // const removeTodoHandler = (todoId: string) => {
-  //   setTodos((prevTodos) => {
-  //     return prevTodos.filter(todo => todo.id !== todoId);
-  //   });
-  // }
-
-
   return (
-    // <div>
-    //   <NewTodo onAddTodo={addTodo}/>
-    //   <Todos 
-    //     items={items} 
-    //     onRemoveTodo={removeTodo}
-    //   />
-    // </div>
     <TodosContextProvider>
       <NewTodo/>
       <Todos />
