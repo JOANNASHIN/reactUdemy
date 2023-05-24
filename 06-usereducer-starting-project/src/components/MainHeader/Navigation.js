26bbbfdb3e11ba9279ad76f20f88a121ae17867a
JOANNASHIN <joannashin3@gmail.com>
2023-05-24 17:25:02 +0900

    context 사용방법 2가지

--- 06-usereducer-starting-project/src/components/MainHeader/Navigation.js
+++ 06-usereducer-starting-project/src/components/MainHeader/Navigation.js
@@ -1,37 +1,69 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import classes from './Navigation.module.css';
 import AuthContext from '../../contexts/auth-context';
 
+/**
+ * context 방법 1
+ */
 const Navigation = (props) => {
+  const ctx = useContext(AuthContext);
+
   return (
-    <AuthContext.Consumer>
-      {(ctx) => {
-        return (
-          <nav className={classes.nav}>
-            <ul>
-              {ctx.isLoggedIn && (
-                <li>
-                  <a href="/">Users</a>
-                </li>
-              )}
-              {ctx.isLoggedIn && (
-                <li>
-                  <a href="/">Admin</a>
-                </li>
-              )}
-              {ctx.isLoggedIn && (
-                <li>
-                  <button onClick={props.onLogout}>Logout</button>
-                </li>
-              )}
-            </ul>
-          </nav>
-        )
-      }}
-      
-    </AuthContext.Consumer>
+    <nav className={classes.nav}>
+      <ul>
+        {ctx.isLoggedIn && (
+          <li>
+            <a href="/">Users</a>
+          </li>
+        )}
+        {ctx.isLoggedIn && (
+          <li>
+            <a href="/">Admin</a>
+          </li>
+        )}
+        {ctx.isLoggedIn && (
+          <li>
+            <button onClick={props.onLogout}>Logout</button>
+          </li>
+        )}
+      </ul>
+    </nav>
   );
 };
+
+/**
+ * context 방법 2
+ */
+// const Navigation = (props) => {
+//   return (
+//     <AuthContext.Consumer>
+//       {(ctx) => {
+//         return (
+//           <nav className={classes.nav}>
+//             <ul>
+//               {ctx.isLoggedIn && (
+//                 <li>
+//                   <a href="/">Users</a>
+//                 </li>
+//               )}
+//               {ctx.isLoggedIn && (
+//                 <li>
+//                   <a href="/">Admin</a>
+//                 </li>
+//               )}
+//               {ctx.isLoggedIn && (
+//                 <li>
+//                   <button onClick={props.onLogout}>Logout</button>
+//                 </li>
+//               )}
+//             </ul>
+//           </nav>
+//         )
+//       }}
+      
+//     </AuthContext.Consumer>
+//   );
+// };
 
 export default Navigation;
